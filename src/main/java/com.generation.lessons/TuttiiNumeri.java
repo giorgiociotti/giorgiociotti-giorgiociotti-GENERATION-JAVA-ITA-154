1836fab87f7ec7eac2f1917bf82e10f3398eb40d
Giorgio <gciotti29@gmail.com>
2026-05-05 10:59:42 +0200

    tuttti i numeri sulla stessa riga fino a 1000
    Signed-off-by: Giorgio <gciotti29@gmail.com>

---
+++ src/main/java/com.generation.lessons/TuttiiNumeri.java
@@ -0,0 +1,9 @@
+package com.generation.lessons;
+
+public class TuttiiNumeri {
+    public static void main(String[] args) {
+        for (int n = 1; n < 1000; n += 2) {
+            System.out.print(n + "-" + (n + 1) + "  ");
+        }
+    }
+}
