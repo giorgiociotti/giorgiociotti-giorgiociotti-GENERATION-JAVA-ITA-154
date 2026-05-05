38c2ed78b67234f8c41e1aff016794c7750b42d9
Giorgio <gciotti29@gmail.com>
2026-05-05 17:30:25 +0200

    MathUtils fatta
    Signed-off-by: Giorgio <gciotti29@gmail.com>

---
+++ src/main/java/com.generation.lessons/MathUtils.java
@@ -0,0 +1,50 @@
+package com.generation.lessons;
+
+public class MathUtils {
+    
+    public static int maxInt(int a, int b) {
+        return (a > b) ? a : b;
+    }
+    
+    public static int minInt(int a, int b) {
+        return (a < b) ? a : b;
+    }
+    
+    public static double minDouble(double a, double b) {
+        return (a < b) ? a : b;
+    }
+    
+    public static double maxDouble(double a, double b) {
+        return (a > b) ? a : b;
+    }
+    
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public static boolean isInRangeInt(int n, int min, int max) {
+        return n >= min && n <= max;
+    }
+    
+    public static boolean isInRangeDouble(double n, double min, double max) {
+        return n >= min && n <= max;
+    }
+    
+    
+}
