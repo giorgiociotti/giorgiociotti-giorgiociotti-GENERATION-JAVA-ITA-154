b4c3ddb0c353805020a2a563cc055300e74dc56e
Giorgio <gciotti29@gmail.com>
2026-05-05 10:52:17 +0200

    numeri pari  da 2 a 1000
    Signed-off-by: Giorgio <gciotti29@gmail.com>

---
+++ src/main/java/com.generation.lessons/NumeriPari.java
@@ -0,0 +1,10 @@
+package com.generation.lessons;
+
+public class NumeriPari {
+    
+    public static void main(String[] args) {
+        for (int i = 2; i < 1000; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
