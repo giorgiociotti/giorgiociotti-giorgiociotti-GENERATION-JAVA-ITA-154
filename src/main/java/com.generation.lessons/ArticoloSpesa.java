508a2145365eeb3ae2d3678cad21ff3dbcf8d2e3
Giorgio <gciotti29@gmail.com>
2026-05-08 11:43:14 +0200

    ArticoloSpesa
    Signed-off-by: Giorgio <gciotti29@gmail.com>

--- src/main/java/com.generation.lessons/ArticoloSpesa.java
+++ src/main/java/com.generation.lessons/ArticoloSpesa.java
@@ -8,84 +8,115 @@ public class ArticoloSpesa {
     boolean acquistato;
     
     
-    
-    public ArticoloSpesa (){}
-    
-    
-    
-      public ArticoloSpesa (String nome,String categoria,double prezzo){
-    this.nome=nome;
-    this.categoria=categoria;
-    this.prezzoUnitario=prezzo;
+    public ArticoloSpesa() {
     }
     
     
-    public ArticoloSpesa (String nome){
-    this.nome=nome;
-    }
-     public  ArticoloSpesa(String nome, String categoria, Double prezzo, int quantita, boolean acquistato){
-        this.nome=nome;
-        this.categoria=categoria;
-        this.prezzoUnitario=prezzo;
-        this.quantita=quantita;
-        this.acquistato=acquistato;
-     }
-    
-    public  String ToString(){
-        return "nome:"+nome+"\ncategoria"+categoria+" \nprezzo"+prezzoUnitario+" \nquantita"+quantita+" \nacquistato"+acquistato;
+    public ArticoloSpesa(String nome, String categoria, double prezzo) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.prezzoUnitario = prezzo;
     }
     
-    public double calcolaTotaleArticolo(){
-        return this.prezzoUnitario*this.quantita;
+    
+    public ArticoloSpesa(String nome) {
+        this.nome = nome;
     }
     
-    void aumentaQuantita(int quantitaDaAggiungere){
-        if(quantitaDaAggiungere<=0){
+    public ArticoloSpesa(String nome, String categoria, Double prezzo, int quantita, boolean acquistato) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.prezzoUnitario = prezzo;
+        this.quantita = quantita;
+        this.acquistato = acquistato;
+    }
+    
+    public String ToString() {
+        return "nome:" + nome + "\ncategoria" + categoria + " \nprezzo" + prezzoUnitario + " \nquantita" + quantita + " \nacquistato" + acquistato;
+    }
+    
+    public double calcolaTotaleArticolo() {
+        return this.prezzoUnitario * this.quantita;
+    }
+    
+    void aumentaQuantita(int quantitaDaAggiungere) {
+        if (quantitaDaAggiungere <= 0) {
             System.out.println("ERRORE");
             return;
         }
-        quantita+=quantitaDaAggiungere;
-        System.out.println("quantita aggiunta ="+ quantita);
+        quantita += quantitaDaAggiungere;
+        System.out.println("quantita aggiunta =" + quantita);
     }
     
-    void ridurreQuantita(int quantitaDaRidurre){
-        if(quantitaDaRidurre<=0){
+    void ridurreQuantita(int quantitaDaRidurre) {
+        if (quantitaDaRidurre <= 0) {
             System.out.println("ERRORE");
             return;
         }
         
-        if ((quantita-quantitaDaRidurre)<=0){
+        if ((quantita - quantitaDaRidurre) <= 0) {
             System.out.println("ERRORE");
             return;
             
         }
         
-        quantita-=quantitaDaRidurre;
-        System.out.println("quantita aggiornata ="+ quantita);
+        quantita -= quantitaDaRidurre;
+        System.out.println("quantita aggiornata =" + quantita);
     }
-    void aggiornaPrezzoUnitario(double nuovoPrezzo){
-        if(nuovoPrezzo<0){
+    
+    void aggiornaPrezzoUnitario(double nuovoPrezzo) {
+        if (nuovoPrezzo < 0) {
             System.out.println("ERRORE");
             return;
         }
-        this.prezzoUnitario=nuovoPrezzo;
+        this.prezzoUnitario = nuovoPrezzo;
         System.out.println("prezzo aggiornato");
         
     }
     
     
     void applicaSconto(double percentuale) {
-    if (percentuale < 0) {
-        System.out.println("ERRORE");
-        return;
-    } else if (percentuale > 100) {
-        System.out.println("ERRORE");
-        return;
+        if (percentuale < 0) {
+            System.out.println("ERRORE");
+            return;
+        } else if (percentuale > 100) {
+            System.out.println("ERRORE");
+            return;
+        }
+        
+        double sconto = prezzoUnitario * (percentuale / 100);
+        prezzoUnitario -= sconto;
+        System.out.println("Sconto applicato. Nuovo prezzo: " + prezzoUnitario);
     }
     
-    double sconto = prezzoUnitario * (percentuale / 100);
-    prezzoUnitario -= sconto;
-    System.out.println("Sconto applicato. Nuovo prezzo: " + prezzoUnitario);
-}
+    void segnaComeAcquistato() {
+        acquistato = true;
+    }
+    
+    void segnaComeDaAcquistare() {
+        acquistato = false;
+    }
+    
+    boolean isCostoso() {
+        return calcolaTotaleArticolo() > 20.0;
+        
+    }
+    
+    static boolean prezzoValido(double prezzo) {
+        return prezzo >= 0;
+    }
+    
+    static boolean quantitaValida(int quantita) {
+        return quantita > 0;
+    }
+    
+    static ArticoloSpesa articoloConTotaleMaggiore(ArticoloSpesa a1, ArticoloSpesa a2) {
+        if (a1.calcolaTotaleArticolo() > a2.calcolaTotaleArticolo()) {
+            
+            return a1;
+        }
+        return a2;
+    }
+    
     
 }
