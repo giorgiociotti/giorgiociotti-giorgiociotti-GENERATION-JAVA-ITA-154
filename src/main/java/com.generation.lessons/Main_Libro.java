e743f2d880a32d6761b08117bcbdc2f19f141942
Giorgio <gciotti29@gmail.com>
2026-05-11 18:05:38 +0200

    Libro e main
    Signed-off-by: Giorgio <gciotti29@gmail.com>

--- src/main/java/com.generation.lessons/Main_Libro.java
+++ src/main/java/com.generation.lessons/Main_Libro.java
@@ -2,7 +2,6 @@ package com.generation.lessons;
 
 public class Main_Libro {
     public static void main(String[] args) {
-        System.out.println("========== TEST CLASSE LIBRO ==========\n");
         
         System.out.println(">> Costruttore vuoto:");
         Libro l1 = new Libro();
@@ -41,6 +40,5 @@ public class Main_Libro {
         
         System.out.println("\n>> Totale libri creati (proprietà statica): " + Libro.getContatoreLibri());
         
-        System.out.println("\n========== FINE TEST ==========");
     }
 }
