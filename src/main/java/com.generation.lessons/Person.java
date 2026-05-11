9e0298be0c01330df2a979d4ac1add9e64a9a742
Giorgio <gciotti29@gmail.com>
2026-05-11 10:05:28 +0200

    addded getter and setterat Person
    Signed-off-by: Giorgio <gciotti29@gmail.com>

--- src/main/java/com.generation.lessons/Person.java
+++ src/main/java/com.generation.lessons/Person.java
@@ -1,23 +1,82 @@
 package com.generation.lessons;
 
 public class Person {
-    String nome;
-    String cognome;
-    String giornoDiCompleanno;
-    String genere;
+    
+    private String nome;
+    private String cognome;
+    private String giornoDiCompleanno;
+    private String genere;
     
     public Person(String n, String c, String gdb, String g) {
-        nome = n;
-        cognome = c;
-        giornoDiCompleanno = gdb;
-        genere = g;  // parametro g → proprietà genere
+        setNome(n);
+        setCognome(c);
+        setGiornoDiCompleanno(gdb);
+        setGenere(g);
+    }
+    
+    
+    public String getNome() {
+        if (nome == null) {
+            return "";
+        }
+        return nome;
+    }
+    
+    public String getCognome() {
+        if (cognome == null) {
+            return "";
+        }
+        return cognome;
+    }
+    
+    public String getGiornoDiCompleanno() {
+        if (giornoDiCompleanno == null) {
+            return "";
+        }
+        return giornoDiCompleanno;
+    }
+    
+    public String getGenere() {
+        if (genere == null) {
+            return "";
+        }
+        return genere;
+    }
+    
+    
+    public void setNome(String nome) {
+        if (nome == null) {
+            return;
+        }
+        this.nome = nome;
+    }
+    
+    public void setCognome(String cognome) {
+        if (cognome == null) {
+            return;
+        }
+        this.cognome = cognome;
+    }
+    
+    public void setGiornoDiCompleanno(String giornoDiCompleanno) {
+        if (giornoDiCompleanno == null) {
+            return;
+        }
+        this.giornoDiCompleanno = giornoDiCompleanno;
+    }
+    
+    public void setGenere(String genere) {
+        if (genere == null) {
+            return;
+        }
+        this.genere = genere;
     }
     
     @Override
     public String toString() {
-        return "Nome: " + nome +
-                       ", Cognome: " + cognome +
-                       ", Compleanno: " + giornoDiCompleanno +
-                       ", Genere: " + genere;
+        return "Nome: " + getNome() +
+                       ", Cognome: " + getCognome() +
+                       ", Compleanno: " + getGiornoDiCompleanno() +
+                       ", Genere: " + getGenere();
     }
 }
